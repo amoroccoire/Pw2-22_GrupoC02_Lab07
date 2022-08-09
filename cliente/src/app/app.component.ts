dfd9acfda3d48f399f097b92c5f209192a38e35e
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-09 01:48:05 -0500

    componente ts

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
   title = 'cliente';
+
+  products = [{nombre:'Producto',categoria:0,tipo:'Default', precio:0, disponible:true, imagen:'Default'}];
+
+  constructor(private api:ApiService){
+    this.getProducts();
+  }
+
+  getProducts = () => {
+    this.api.getAllProducts().subscribe (
+      data => {
+        console.log(data);
+        this.products = data;  //data.results;
+      },
+      error => {
+        console.log(error);
+      })  
+
+    } 
+
 }
