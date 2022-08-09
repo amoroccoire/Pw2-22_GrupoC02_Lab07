fd5ca33116fe74ea81ab202cd2667c93c455cfd9
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-08 23:24:32 -0500

    agrego las vistas api producto get y post

--- servidor/api/views.py
+++ servidor/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import permissions
+from producto.models import Producto
+from .serializers import ProductoSerializer
 
 # Create your views here.
+
+class ProductoListApiView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        productos = Producto.objects.all()
+        serializer = ProductoSerializer(productos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            'nombre': request.data.get('nombre'), 
+            'categoria': request.data.get('categoria'),
+            'tipo': request.data.get('tipo'),
+            'precio': request.data.get('precio'),
+            'disponible': request.data.get('disponible'),
+            'imagen': request.data.get('imagen'),
+        }
+        serializer = ProductoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
