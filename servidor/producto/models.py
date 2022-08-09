3c9c457372a69e30d22ffee31ada1b30a3b08a3a
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-08-08 22:09:01 -0500

    creando el modelo producto

--- servidor/producto/models.py
+++ servidor/producto/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+  COMIDA = 1
+  POSTRE = 2
+  BEBIDA = 3
+  ENTRADA = 4
+
+  CATEGORIAS = (
+    (COMIDA, "Comida"),
+    (POSTRE, "Postre"),
+    (BEBIDA, "Bebida"),
+    (ENTRADA, "Entrada"),
+  )
+
+  nombre = models.TextField()
+  categoria = models.IntegerField(choices=CATEGORIAS)
+  tipo = models.TextField()
+  precio = models.IntegerField()
+  disponible = models.BooleanField()
+  imagen = models.TextField()
